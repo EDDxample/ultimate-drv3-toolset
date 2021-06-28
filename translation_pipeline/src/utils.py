1a8a85db0c966971d8487ee14d9f5bc475290c93
EDD <EDDxample@gmail.com>
2021-06-28 02:39:22 +0200

    remove unused functions from utils

--- translation_pipeline/src/utils.py
+++ translation_pipeline/src/utils.py
@@ -65,53 +65,6 @@ def write(mode: str, data: Any, bigendian: bool = False, encoding: str = 'utf-8'
         return data.encode(encoding)[2:] + b'\x00\x00'
 
 
-def read_string(data: BufferedReader, length, encoding='utf-8'):
-    return data.read(length).decode(encoding)
-    
-def read_null_terminated_string(data: BufferedReader, encoding='utf-16'):
-    string = bytes()
-    while True:
-        if encoding == 'utf-16':
-            char = data.read(2)
-            if char == b'\x00\x00' or not char:
-                return string.decode(encoding)
-            else: string += char
-        else:
-            char = data.read(1)
-            if char == b'\x00' or not char:
-                return string.decode(encoding)
-            else: string += char
-            
-
-def read_u32(data: BufferedReader, bigendian=False):
-    endianness = '>' if bigendian else '<'
-    return struct.unpack(endianness + 'I', data.read(4))[0]
-
-def read_u16(data: BufferedReader, bigendian=False):
-    endianness = '>' if bigendian else '<'
-    return struct.unpack(endianness + 'H', data.read(2))[0]
-
-def read_ubyte(data: BufferedReader, bigendian=False):
-    endianness = '>' if bigendian else '<'
-    return struct.unpack(endianness + 'B', data.read(1))[0]
-
-def write_null_terminated_string(string: str) -> bytes:
-    return string.encode('utf-16')[2:] + b'\x00\x00' # remove endianness mark 0xFFFE
-
-def write_u32(n: int, bigendian=False) -> bytes:
-    endianness = '>' if bigendian else '<'
-    return struct.pack(endianness + 'I', n)
-
-def write_u16(n: int, bigendian=False) -> bytes:
-    endianness = '>' if bigendian else '<'
-    return struct.pack(endianness + 'H', n)
-
-def write_ubyte(n: int, bigendian=False):
-    endianness = '>' if bigendian else '<'
-    return struct.pack(endianness + 'B', n)
-
-
-
 def ensure_paths(filename):
     if not os.path.exists(os.path.dirname(filename)):
         try:
